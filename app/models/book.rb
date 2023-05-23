6cb85365cbf2aec1ed44e84acf89dde632fe76bf
EC2 Default User <ec2-user@ip-172-31-12-15.ap-northeast-1.compute.internal>
2023-05-23 11:15:30 +0000

    [Add]課題3 Bootstrapインストール、アソシエーション設定

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ApplicationRecord
   has_one_attached :image
   
+  belongs_to :user
+  
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   
-  resources :books 
+  
   root to: "homes#top" 
   devise_for :users
+  
+  resources :books
+  resources :users, only: [:show, :edit] 
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
