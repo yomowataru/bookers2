e6bb3ff8cd10d60520cd5c3c24331e965e4d464a
EC2 Default User <ec2-user@ip-172-31-12-15.ap-northeast-1.compute.internal>
2023-05-23 12:59:37 +0000

    [Add]課題3 ユーザー名でログインする機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   
-  
-  root to: "homes#top" 
   devise_for :users
+  root to: "homes#top" 
+  get '/home/about' => 'homes#about', as: 'about'
   
   resources :books
-  resources :users, only: [:show, :edit] 
+  resources :users, only: [:index, :show, :edit] 
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
