8e66c03bf4e3364fe32f0fb849eddc37d29184e9
EC2 Default User <ec2-user@ip-172-31-12-15.ap-northeast-1.compute.internal>
2023-05-24 14:24:30 +0000

    [Add]課題3 部分テンプレート作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+    @user = User.find(params[:id]) 
   end
 
   def new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,12 @@ class UsersController < ApplicationController
   end
   
   def show
+    @user = User.find(params[:id]) 
+    @books = @user.books 
   end
 
   def edit
+    @user = User.find(params[:id]) 
+    
   end
 end
