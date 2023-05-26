78f259e124671954d330dfdfdf6d71c988467a8b
EC2 Default User <ec2-user@ip-172-31-12-15.ap-northeast-1.compute.internal>
2023-05-26 10:02:11 +0000

    [Add]課題3 bookの更新・削除機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,9 +13,26 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @books = Book.find(params[:id])
+  end
+  
+  def update
+    @books = Book.find(params[:id])
+    @books.update(book_params)
+    redirect_to book_path(@books.id)
+    
   end
 
   def show
+    @book = Book.new
+    @user = current_user
+    @books = Book.find(params[:id]) 
+  end
+  
+  def destroy
+    books = Book.find(params[:id]) 
+    books.destroy 
+    redirect_to books_path
   end
 
   private
