7ffa1c2cbb8d57aeb551594c6447943dd6d61a11
EC2 Default User <ec2-user@ip-172-31-12-15.ap-northeast-1.compute.internal>
2023-05-26 13:40:11 +0000

    [Add]課題3 userのindex,showページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,32 @@
 class UsersController < ApplicationController
   def index
     @user = current_user
+    @users = User.all
+    
   end
   
   def create
+    @book = Book.new(book_params) 
+    @book.user_id = current_user.id 
+    @book.save 
+    redirect_to books_path 
   end
   
   def show
-    @user = User.find(params[:id]) 
+    @user = current_user
+    @users = User.find(params[:id]) 
+    @book = Book.new 
     @books = @user.books 
   end
 
   def edit
     @user = User.find(params[:id]) 
-    
   end
+  
+  private
+
+  def book_params 
+    params.require(:book).permit(:title, :body, :user_id) 
+  end
+  
 end
