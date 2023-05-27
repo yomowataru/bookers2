758ed33c2bf941610bf78645c145326e5c73f76b
EC2 Default User <ec2-user@ip-172-31-12-15.ap-northeast-1.compute.internal>
2023-05-27 01:44:09 +0000

    [Add]課題3 header Font Awesome設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,10 +23,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id]) 
   end
   
+  def update
+    @user = User.find(params[:id]) 
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+  
   private
 
   def book_params 
     params.require(:book).permit(:title, :body, :user_id) 
   end
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end 
   
 end
